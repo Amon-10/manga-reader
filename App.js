35e7c43ab4e12cb9a6ad8947d0e62de7a5546211
Aman <alal09635@gmail.com>
2025-06-28 18:17:01 -0700

    initial commit

---
+++ App.js
@@ -0,0 +1,28 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Manga Reader MVP</Text>
+      <Text style={styles.subtitle}>You're up and running!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fonttSize: 28,
+    fontWeight: 'bold',
+  },
+  subtitle: {
+    fontSize: 16,
+    marginTop: 10,
+    color: '#666',
+  },
+});
