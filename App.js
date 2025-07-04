481c7405c797b2eb5f8377c711511ee7ff9046df
Aman <alal09635@gmail.com>
2025-07-04 11:29:50 -0700

    Added hero image and signup button

--- App.js
+++ App.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Manga Reader MVP boom</Text>
-      <Text style={styles.subtitle}>You're up and running!</Text>
+      <Image
+        source={{ uri: 'https://images.unsplash.com/photo-1548827752-6301e20b3be0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fG1hbmdhfGVufDB8fDB8fHww'}}
+        style={styles.heroImage}
+      />
+      <Text style={styles.title}>Welcome to the Manga Reader!</Text>
+      <Text style={styles.subtitle}>Browse Thousands of manga</Text>
       {<Button
         title="Go to browse"
         onPress={() => console.log("Browse screen placeholder")}
       />}
+      <View style={{marginTop: 10}}>
+        <Button 
+          title="Get Started" 
+          onPress={() => console.log("Signup placeholder")} />
+      </View>
     </View>
   );
 }
@@ -20,6 +29,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+  },
+  heroImage: {
+    width:'100%',
+    height: 150,
+    marginBottom:20,
+    resizeMode: 'cover',
+    borderRadius: 20,
   },
   title: {
     fonttSize: 28,
@@ -29,5 +46,6 @@ const styles = StyleSheet.create({
     fontSize: 16,
     marginTop: 10,
     color: '#666',
+    marginBottom: 10,
   },
 });
