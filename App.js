007ccf240fbd4c6db22172fd5f9239129c554a88
Aman <alal09635@gmail.com>
2025-07-05 16:16:35 -0700

    built navigation bottom bar

--- App.js
+++ App.js
@@ -1,51 +1,52 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
+/* import * as React from 'react'; */
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { View, Text } from 'react-native';
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+
+function LibraryScreen() {
   return (
-    <View style={styles.container}>
-      <Image
-        source={{ uri: 'https://images.unsplash.com/photo-1548827752-6301e20b3be0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fG1hbmdhfGVufDB8fDB8fHww'}}
-        style={styles.heroImage}
-      />
-      <Text style={styles.title}>Welcome to the Manga Reader!</Text>
-      <Text style={styles.subtitle}>Browse Thousands of manga</Text>
-      {<Button
-        title="Go to browse"
-        onPress={() => console.log("Browse screen placeholder")}
-      />}
-      <View style={{marginTop: 10}}>
-        <Button 
-          title="Get Started" 
-          onPress={() => console.log("Signup placeholder")} />
-      </View>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Library</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  heroImage: {
-    width:'100%',
-    height: 150,
-    marginBottom:20,
-    resizeMode: 'cover',
-    borderRadius: 20,
-  },
-  title: {
-    fonttSize: 28,
-    fontWeight: 'bold',
-  },
-  subtitle: {
-    fontSize: 16,
-    marginTop: 10,
-    color: '#666',
-    marginBottom: 10,
-  },
-});
+function BrowseScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Browse</Text>
+    </View>
+  );
+}
+
+function HistoryScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>History</Text>
+    </View>
+  );
+}
+
+function MoreScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>More</Text>
+    </View>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator initialRouteName='Library'>
+        <Tab.Screen name="Library" component={LibraryScreen}/>
+        <Tab.Screen name="History" component={HistoryScreen}/>
+        <Tab.Screen name="Browse" component={BrowseScreen}/>
+        <Tab.Screen name="More" component={MoreScreen}/>
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
