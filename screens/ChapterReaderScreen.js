0cdc572485fae47e684228f22dcdf088ab600595
Aman <alal09635@gmail.com>
2025-08-03 03:24:34 -0700

    resized chapter images to fit screen better and added error message in ui in case images arent loaded

--- screens/ChapterReaderScreen.js
+++ screens/ChapterReaderScreen.js
@@ -1,10 +1,13 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text, Image, ScrollView, ActivityIndicator} from 'react-native';
+import {View, Text, Image, ScrollView, ActivityIndicator, Dimensions} from 'react-native';
+
+const screenWidth = Dimensions.get('window').width;
 
 export default function ChapterReaderScreen({route}){
     const {chapter} = route.params;
     const [pages, setPages] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
 
     useEffect(() => {
         const fetchPages = async () => {
@@ -16,6 +19,7 @@ export default function ChapterReaderScreen({route}){
                 
             } catch(err){
                 console.error('Error fetching chapter images', err);
+                setError(true);
             } finally {
                 setLoading(false);
             }
@@ -25,7 +29,22 @@ export default function ChapterReaderScreen({route}){
     }, [chapter.hid]); // the stuff in the brackets make it so that this runs 
     // once when the component first mounts and then again when chapter.hid changes
 
-    if (loading) return <ActivityIndicator size='large' color='gray'/>;
+    if (loading) {
+        return (
+            <View style={{flex:1, justifyContent: 'center', alignItems: 'center', padding: 16}}>
+                <ActivityIndicator size='large' color='gray'/>
+                <Text>Loading pages...</Text>
+            </View>
+        );
+    }
+
+    if (error) {
+        return (
+            <View style={{flex:1, justifyContent: 'center', alignItems: 'center', padding: 16}}>
+                <Text style={{ color: 'red' }}>Failed to load chapter. Please try again later.</Text>
+            </View>
+        )
+    }
 
     return (
         <ScrollView>
@@ -33,7 +52,8 @@ export default function ChapterReaderScreen({route}){
                 <Image
                     key={index}
                     source={{ uri: `https://meo.comick.pictures/${page.b2key}`}}
-                    style={{ width: '100%', height: 500, resizeMode: 'contain' }} 
+                    style={{ width: screenWidth, height: screenWidth * 1.4, resizeMode: 'contain', marginBottom: 8, }} 
+                    onError={() => console.warn(`Image ${index + 1} failed to load`)}
                 />
             ))}
         </ScrollView>
