4515d3ab59902a7db3dc3a79471c9068ba0dd6be
Aman <alal09635@gmail.com>
2025-08-16 20:11:39 -0700

    reorganized manga details section and added more placeholders for manga details

--- screens/MangaDetailsScreen.js
+++ screens/MangaDetailsScreen.js
@@ -1,5 +1,5 @@
 import React,{ useEffect, useLayoutEffect, useState } from 'react';
-import {View, Text, FlatList, Image, TouchableOpacity} from 'react-native';
+import {View, Text, FlatList, Image, TouchableOpacity, ScrollView} from 'react-native';
 import { useRoute } from '@react-navigation/native';
 import { Ionicons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
@@ -62,47 +62,59 @@ export default function MangaDetailsScreen(){
     fetchMangaDetails();
   }, []);
 
+  const renderHeader = () => (
+    <View style={{ flex: 1, justifyContent: 'flex-start', marginTop: 20}}>
+          <View style={{ flexDirection: "row"}}>
+            <View style={{alignItems: 'flex-start', paddingLeft: 10, paddingRight: 10}}>
+              <Image
+                source={{uri: getCoverUrl(manga) || 'https://via.placeholder.com/150'}}
+                style={{ width: 120, height:170, borderRadius: 7 }}
+              />
+            </View>
+            <View style={{ justifyContent: 'center', width: '60%' }}>
+              <Text style={{fontSize: 23}}>{manga.title}</Text>
+              <Text style={{fontSize: 15}}>Author: {manga.id}</Text>
+              <Text style={{fontSize: 15}}>Status:</Text>
+              <Text style={{fontSize: 15}}>Comick</Text>
+            </View>
+          </View>
+              <Text style={{marginTop: 10}}>{manga.desc}</Text>
+          
+    </View>
+  );
+
     return (
-        <View style={{ flex: 1, justifyContent: 'flex-start', marginTop: 20}}>
-          <View style={{alignItems: 'center'}}>
-            <Image
-              source={{uri: getCoverUrl(manga) || 'https://via.placeholder.com/150'}}
-              style={{ width: 120, height:170, borderRadius: 7 }}
-            />
-
-            <Text style={{fontSize: 20, marginTop: 10}}>{manga.title}</Text>
-
-            <Text style={{marginTop: 10}}>summary</Text>
-          </View>
-
-          <FlatList
-            data={chapterList}
-            keyExtractor={(item) => item.hid}
-            renderItem={({item}) => (
-              <TouchableOpacity onPress={() => {navigation.navigate('ChapterReader', {chapter: item})}}
-                style={{ width: '100%', padding: 5, marginVertical: 2, backgroundColor: '#f2f2f2', left: 18 }}>
-                <Text style={{fontSize: 16}}>chapter {item.chap}</Text>
-                <Text style={{fontSize: 12}}>date added: {item.created_at.slice(0,10)}</Text>
-              </TouchableOpacity>
-            )}
-          />
-          <View style={{position: 'absolute', bottom: 30, right: 10, alignItems: 'center', elevation: 5, zIndex: 100}}>
-            <TouchableOpacity onPress={() => alert('Read now button')}
-            style={{ 
-              flexDirection: 'row', 
-              alignItems:'center', 
-              backgroundColor:'red', 
-              padding: 15, 
-              borderRadius: 7, 
-            }} >
-
-              <Ionicons name='caret-forward-outline' color='white' size= {25}/>
-
-              <Text style={{color: 'white', fontSize: 12, marginLeft: 10}}>Read</Text>
-
+      <View>
+        <FlatList
+          data={chapterList}
+          keyExtractor={(item) => item.hid}
+          renderItem={({item}) => (
+            <TouchableOpacity onPress={() => {navigation.navigate('ChapterReader', {chapter: item})}}
+              style={{ width: '100%', padding: 5, marginVertical: 2, backgroundColor: '#f2f2f2', left: 18 }}>
+              <Text style={{fontSize: 16}}>chapter {item.chap}</Text>
+              <Text style={{fontSize: 12}}>date added: {item.created_at.slice(0,10)}</Text>
             </TouchableOpacity>
-          </View>
-        </View>
+          )}
+
+          ListHeaderComponent={renderHeader}
+        />
         
-      );
+        <View style={{position: 'absolute', bottom: 30, right: 10, alignItems: 'center', elevation: 5, zIndex: 100}}>
+          <TouchableOpacity onPress={() => alert('Read now button')}
+          style={{ 
+            flexDirection: 'row', 
+            alignItems:'center', 
+            backgroundColor:'red', 
+            padding: 15, 
+            borderRadius: 7, 
+          }} >
+
+            <Ionicons name='caret-forward-outline' color='white' size= {25}/>
+
+            <Text style={{color: 'white', fontSize: 12, marginLeft: 10}}>Read</Text>
+
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
 }
