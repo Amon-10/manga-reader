a82f18da85beee8803b1cf7a9450b63bcbf2ca90
Aman <alal09635@gmail.com>
2025-08-25 11:45:18 -0700

    created database tables for library and history

---
+++ mangaReader/database/database.js
@@ -0,0 +1,27 @@
+import * as SQLite from 'expo-sqlite';
+
+const db = await SQLite.openDatabaseAsync('mangaReader.db');
+
+export const initDB = () => {
+    db.transaction (tx => {
+        tx.executeSql(
+            `CREATE TABLE IF NOT EXISTS library (
+                mangaId TEXT PRIMARY KEY NOT NULL,
+                title TEXT,
+                coverURL TEXT,
+                lastReadChapter TEXT
+            );`
+        );
+
+        tx.executeSql(
+            `CREATE TABLE IF NOT EXISTS history (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                mangaId TEXT NOT NULL,
+                chapterId TEXT,
+                lastReadPage INTEGER
+            );`
+        );
+    });
+};
+
+export default db;
