3b1a27ebe10cf7d760dd3a1a03a91720a468355e
Aman <alal09635@gmail.com>
2025-08-31 11:19:22 -0700

    renamed database and App to App and MyApp respectively, and properly imported it

--- mangaReader/App.js
+++ mangaReader/App.js
@@ -1,62 +1,24 @@
-import React, {useState} from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Ionicons } from '@expo/vector-icons';
-import { View, Text, FlatList, Image, TouchableOpacity } from 'react-native';
-import { HistoryScreen, MoreScreen} from './screens';
-import BrowseStack from './screens/BrowseStack';
-import LibraryStack from './screens/LibraryStack';
-import { StatusBar } from 'expo-status-bar';
+import {SQLiteProvider} from 'expo-sqlite';
+import { MyApp } from './MyApp';
 
-const Tab = createBottomTabNavigator();
-
-export default function App() {
-  const [mangaList, setMangaList] = useState([]);
-  const [libraryList, setLibraryList] = useState([]);
-  
-  return (
-    <NavigationContainer>
-      <StatusBar style="dark" />
-      <Tab.Navigator initialRouteName='Library'
-      screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-
-            if (route.name === 'Library') {
-              iconName = focused ? 'book' : 'book-outline';
-            }
-            else if (route.name ==='History') {
-              iconName = focused ? 'timer' : 'timer-outline';
-            } 
-            else if (route.name === 'Browse') {
-              iconName = focused ? 'search' : 'search-outline';
-            }
-            else if (route.name === 'More') {
-              iconName = focused ? 'ellipsis-horizontal' : 'ellipsis-horizontal-outline';
-            }
-
-            // Return the icon component
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-          tabBarActiveTintColor: 'tomato',
-          tabBarInactiveTintColor: 'gray',
-        })}
+export default function App(){
+    return(
+        <SQLiteProvider 
+            databaseName='Library.db'
+            onInit={async (db) => {
+                await db.execAsync(`
+                    CREATE TABLE IF NOT EXISTS library (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        mangaId TEXT UNIQUE,
+                        cover TEXT,
+                        title TEXT,
+                        slug TEXT
+                    )
+                `);   
+            }}
         >
-
-        <Tab.Screen name="Library" options={{ headerShown: false }}>
-          {() => <LibraryStack libraryList={libraryList} setLibraryList={setLibraryList}/>}
-        </Tab.Screen>
-
-        <Tab.Screen name="History" component={HistoryScreen}/>
-
-        <Tab.Screen name="Browse" options={{ headerShown: false }}>
-          {() => <BrowseStack mangaList={mangaList} setMangaList={setMangaList} libraryList={libraryList} setLibraryList={setLibraryList} /> }
-        </Tab.Screen>
-
-        <Tab.Screen name="More" component={MoreScreen}/> 
-        
-      </Tab.Navigator>
-    </NavigationContainer>
-  ); 
+            < MyApp />
+        </SQLiteProvider>
+    )
 }
 
---
+++ mangaReader/MyApp.js
@@ -0,0 +1,62 @@
+import React, {useState} from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import { View, Text, FlatList, Image, TouchableOpacity } from 'react-native';
+import { HistoryScreen, MoreScreen} from './screens';
+import BrowseStack from './screens/BrowseStack';
+import LibraryStack from './screens/LibraryStack';
+import { StatusBar } from 'expo-status-bar';
+
+const Tab = createBottomTabNavigator();
+
+export default function MyApp() {
+  const [mangaList, setMangaList] = useState([]);
+  const [libraryList, setLibraryList] = useState([]);
+  
+  return (
+    <NavigationContainer>
+      <StatusBar style="dark" />
+      <Tab.Navigator initialRouteName='Library'
+      screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Library') {
+              iconName = focused ? 'book' : 'book-outline';
+            }
+            else if (route.name ==='History') {
+              iconName = focused ? 'timer' : 'timer-outline';
+            } 
+            else if (route.name === 'Browse') {
+              iconName = focused ? 'search' : 'search-outline';
+            }
+            else if (route.name === 'More') {
+              iconName = focused ? 'ellipsis-horizontal' : 'ellipsis-horizontal-outline';
+            }
+
+            // Return the icon component
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+        >
+
+        <Tab.Screen name="Library" options={{ headerShown: false }}>
+          {() => <LibraryStack libraryList={libraryList} setLibraryList={setLibraryList}/>}
+        </Tab.Screen>
+
+        <Tab.Screen name="History" component={HistoryScreen}/>
+
+        <Tab.Screen name="Browse" options={{ headerShown: false }}>
+          {() => <BrowseStack mangaList={mangaList} setMangaList={setMangaList} libraryList={libraryList} setLibraryList={setLibraryList} /> }
+        </Tab.Screen>
+
+        <Tab.Screen name="More" component={MoreScreen}/> 
+        
+      </Tab.Navigator>
+    </NavigationContainer>
+  ); 
+}
+
