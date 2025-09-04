005eff7e6e9a254382210fd1da155ff1cd578258
Aman <alal09635@gmail.com>
2025-09-03 21:09:34 -0700

    set mangaId type to int

--- mangaReader/App.js
+++ mangaReader/App.js
@@ -9,7 +9,7 @@ export default function App(){
                 await db.execAsync(`
                     CREATE TABLE IF NOT EXISTS library (
                         id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        mangaId TEXT UNIQUE,
+                        mangaId INT UNIQUE,
                         cover TEXT NOT NULL,
                         title TEXT NOT NULL,
                         slug TEXT NOT NULL
